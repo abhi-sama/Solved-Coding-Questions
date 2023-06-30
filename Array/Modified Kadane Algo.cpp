b8cd71cd460c6181f4c160973471389dd8f1cc7b
abhi-sama <abhi121197@gmail.com>
2023-06-30 07:44:59 -0400

    Modified Kadane Algo

--- Array/Modified Kadane Algo.cpp
+++ Array/Modified Kadane Algo.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    int startIndex=0;
-    int endIndex=0;
     long long maxSum=-1;
     long long currSum=0;
     //Modified Kadane if there is no postive value
     int maxele=*max_element(nums.begin(),nums.end());
     if(maxele<=0)return maxele;
-
+    int start=-1;
+    int ansStart=-1;
+    int ansEnd=-1;
     for(int i=0;i<nums.size();i++)
-    {
+    { if (currSum==0)start=i;
       if (currSum + nums[i] >= 0) {
         currSum += nums[i];
+        ansStart=start;
+        ansEnd=i;
       }
       else {
-          startIndex=i;
           currSum=0;
       }
         if(currSum>maxSum)
