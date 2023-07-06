dc176aa46ae562b92b07533f3a45c1f907fb9a96
abhi-sama <abhi121197@gmail.com>
2023-07-06 11:16:47 -0400

    Sudoku Solver

--- Codes in C++/Recursion&BackTracking/N-Queens.cpp
+++ Codes in C++/Recursion&BackTracking/N-Queens.cpp
@@ -39,6 +39,59 @@ public:
         return ans;
     }
 };
+//when return type is vector<vector<int>>
+/*
+    void solve(int col,int n,vector<vector<int>> &board,
+    vector<vector<int>> &ans,
+    vector<int> &leftRow,vector<int> &upperDiag,vector<int> &lowerDiag)
+    {
+        if(col==n)
+        {
+        vector<int> temp;
+
+        for(auto it:board)
+
+        {
+
+            for(int i=0;i<it.size();i++)
+
+              temp.push_back(it[i]);
+
+        }  
+
+        ans.push_back(temp);
+        } 
+
+        for(int row=0;row<n;row++)
+        {
+            if(leftRow[row]==0&&upperDiag[n-1 +col-row]==0&&lowerDiag[row+col]==0)
+            {   leftRow[row]=1;
+                upperDiag[n-1 +col-row]=1;
+                lowerDiag[row+col]=1;
+                board[row][col]=1;
+                solve(col+1,n,board,ans,leftRow,upperDiag,lowerDiag);
+                board[row][col]=0;
+                leftRow[row]=0;
+                upperDiag[n-1 + col-row]=0;
+                lowerDiag[row+col]=0;
+            }
+        }
+        return;
+    }
+
+vector<vector<int>> solveNQueens(int n) {
+        vector<vector<int>> board(n,vector<int>(n,0));
+        vector<vector<int>> ans;
+        /*string s(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        vector<int> leftRow(n,0),upperDiag(2*n-1,0),lowerDiag(2*n-1,0);
+        solve(0,n,board,ans,leftRow,upperDiag,lowerDiag);
+        return ans;
+}
+*/
  
  /*bool isSafe1(int row, int col, vector < string > board, int n) {
       // check upper element
---
+++ Codes in Python3/Recursion&BackTracking/Sudoku Solver.py
@@ -0,0 +1,23 @@
+def isValid(board: List[List[str]], row: int, col: int, c: str) -> bool:
+    for i in range(9):
+        if board[i][col] == c:
+            return False
+        if board[row][i] == c:
+            return False
+        if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == c:
+            return False
+    return True
+
+def solveSudoku(board: List[List[str]]) -> bool:
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == ".":
+                for c in "123456789":
+                    if isValid(board, i, j, c):
+                        board[i][j] = c
+                        if solveSudoku(board):
+                            return True
+                        else:
+                            board[i][j] = "."
+                return False
+    return True
