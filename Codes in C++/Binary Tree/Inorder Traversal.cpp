3d2afe48d5561bd32f3439e8bf4e9fdd605527d8
abhi-sama <abhi121197@gmail.com>
2023-07-31 03:57:54 -0400

    Inorder Traversal

---
+++ Codes in C++/Binary Tree/Inorder Traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    stack<TreeNode*> s;
+    while(root!=NULL||!s.empty())
+    {
+        while(root!=NULL)
+        {
+            s.push(root);
+            root=root->left;
+        }
+        root=s.top();
+        s.pop();
+        res.push_back(root->val);
+        root=root->right;
+    }
+    return res;
+    }
+/*public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        inorder(root, nodes);
+        return nodes;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& nodes) {
+        if (!root) {
+            return;
+        }
+        inorder(root -> left, nodes);
+        nodes.push_back(root -> val);
+        inorder(root -> right, nodes);
+    }*/
+};
