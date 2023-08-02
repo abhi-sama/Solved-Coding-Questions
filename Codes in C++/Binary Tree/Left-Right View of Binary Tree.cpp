a67dd28c94ee085a3cdf78efabfa37f549d1d054
abhi-sama <abhi121197@gmail.com>
2023-08-02 16:25:32 -0400

    Left-Right View of Binary Tree

--- Codes in C++/Binary Tree/Left-Right View of Binary Tree.cpp
+++ Codes in C++/Binary Tree/Left-Right View of Binary Tree.cpp
@@ -12,3 +12,32 @@ vector<int> leftView(Node *root)
    left(root,0,res);
    return res;
 }
+
+// vector<int> leftView(Node *root)
+//     {
+//        // Your Code here
+//          vector<int> result;
+//     if (!root)
+//         return result;
+//     queue<Node*> levelQueue;
+//     levelQueue.push(root); 
+//     while (!levelQueue.empty()) {
+//         int levelSize = levelQueue.size(); 
+//               // Traverse all nodes in the current level and keep track of the leftmost node's value
+//                int leftmostValue;
+//           for (int i = 0; i < levelSize; ++i) {
+
+//                  Node* node = levelQueue.front(); 
+//                  levelQueue.pop(); 
+//                  leftmostValue = node-> data;
+            
+//                  if(i==0) result.push_back(leftmostValue); 
+
+//                 if (node->left) 
+//                      levelQueue.push(node->left);
+//                 if (node->right) // same as left
+//                      levelQueue.push(node->right);
+//         }
+//     }
+//     return result;
+//     };
