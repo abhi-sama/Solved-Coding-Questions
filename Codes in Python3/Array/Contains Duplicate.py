14eefe1e9dcd79753ed8d9999ae56cde08c24718
Abhishek Sharma <abhi121197@gmail.com>
2025-09-27 17:28:33 -0700

    Contains Duplicate

---
+++ Codes in Python3/Array/Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen=set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
