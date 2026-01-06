26bce0ada74a579292f6f14b068febcfb1e4ccfa
abhi-sama <abhi121197@gmail.com>
2026-01-05 18:11:02 -0800

    No of Islands

--- Codes in Python3/Graph/No of Islands mXn nodes.py
+++ Codes in Python3/Graph/No of Islands mXn nodes.py
@@ -1,35 +1,79 @@
-##Solution 1
+##Solution 5
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        ROWS,COLS=len(grid),len(grid[0])
+        visited=set()
+        dir=[[-1,0],[0,1],[1,0],[0,-1]]
+        nos_islands=0
 
-        rows, cols= len(grid), len(grid[0])
-        visit=set()
-        islands=0
+        def dfs(i,j):
+            if (i not in range(0,ROWS)) or (j not in range(0,COLS)) or (grid[i][j]=="0"):
+                return 
+            if (i,j) in visited:
+                return
+            visited.add((i,j))
+            for dr,dc in dir:
+                dfs(i+dr,j+dc)
 
-        def bfs(r,c):
-            q=collections.deque()
-            visit.add((r,c))
+        def bfs (r,c):
+            q=deque()
+            visited.add((r,c))
             q.append((r,c))
-            while q:
-                r,c = q.popleft()
-                directions=[[-1,0],[0,1],[1,0],[0,-1]]
-                for dr,dc in directions:
-                    nrow, ncol= r+dr, c+dc
-                    if(nrow in range(rows) and
-                       ncol in range(cols) and
-                       grid[nrow][ncol] == "1" and 
-                       (nrow,ncol) not in visit ):
-                       q.append((nrow,ncol))
-                       visit.add((nrow,ncol))
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c]=="1" and (r,c) not in visit:
+            while q:
+                row,col=q.popleft()
+                for dr,dc in dir:
+                    nrow,ncol=row+dr,col+dc
+                    if (0 <= nrow < ROWS and 0 <= ncol < COLS and grid[nrow][ncol] == "1" and (nrow, ncol) not in visited):
+                        q.append((nrow,ncol))
+                        visited.add((nrow,ncol))
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if(grid[r][c]=="1" and (r,c) not in visited):
                     bfs(r,c)
-                    islands+=1
-        return islands
+                    nos_islands+=1 
+        
+        return   nos_islands
+
+            
+
+
+
+##Solution 1
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         if not grid:
+#             return 0
+
+#         rows, cols= len(grid), len(grid[0])
+#         visit=set()
+#         islands=0
+
+#         def bfs(r,c):
+#             q=collections.deque()
+#             visit.add((r,c))
+#             q.append((r,c))
+#             while q:
+#                 r,c = q.popleft()
+#                 directions=[[-1,0],[0,1],[1,0],[0,-1]]
+#                 for dr,dc in directions:
+#                     nrow, ncol= r+dr, c+dc
+#                     if(nrow in range(rows) and
+#                        ncol in range(cols) and
+#                        grid[nrow][ncol] == "1" and 
+#                        (nrow,ncol) not in visit ):
+#                        q.append((nrow,ncol))
+#                        visit.add((nrow,ncol))
+
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c]=="1" and (r,c) not in visit:
+#                     bfs(r,c)
+#                     islands+=1
+#         return islands
+    
 ##Solution 2
 # class Solution:
 #     def numIslands(self, grid: List[List[str]]) -> int:
