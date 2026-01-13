9298891ae54e0d67183a17c58b0362dfc9d18c5e
abhi-sama <abhi121197@gmail.com>
2026-01-12 17:49:27 -0800

    Permutations

--- Codes in Python3/Recursion&BackTracking/Permutations.py
+++ Codes in Python3/Recursion&BackTracking/Permutations.py
@@ -1,32 +1,52 @@
+#Optimal Solution
 class Solution:
-    # def recurPermute(self, nums: List[int], freq: List[int]):
-    #     if len(self.ds) == len(nums):
-    #         self.ans.append(self.ds.copy())
-    #         return
-    #     for i in range(len(nums)):
-    #         if not freq[i]:
-    #             self.ds.append(nums[i])
-    #             freq[i] = 1
-    #             self.recurPermute(nums, freq)
-    #             freq[i] = 0
-    #             self.ds.pop()
-    
-    # def permute(self, nums: List[int]) -> List[List[int]]:
-    #     self.ans = []
-    #     self.ds = []
-    #     freq = [0] * len(nums)
-    #     self.recurPermute(nums, freq)
-    #     return self.ans   
-    def recPermute(self, index: int, nums: List[int], res: List[List[int]]):
-        if index == len(nums):
-            res.append(nums.copy())
-            return
-        for i in range(index, len(nums)):
-            nums[index], nums[i] = nums[i], nums[index]
-            self.recPermute(index + 1, nums, res)
-            nums[index], nums[i] = nums[i], nums[index]
-
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        self.recPermute(0, nums, res)
+        res=[]
+        def backtrack(ind):
+            if ind==len(nums):
+                return res.append(nums.copy())
+            for i in range(ind,len(nums)):
+                nums[ind],nums[i]=nums[i],nums[ind]
+                backtrack(ind+1)
+                nums[ind],nums[i]=nums[i],nums[ind]
+        backtrack(0)
         return res
+
+# TC=O(n!*n)
+# SC=O(n!*n)
+
+#Solution 2
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms=[[]]
+        for n in nums:
+            new_perms=[]
+            for p in perms:
+                for i in range(len(p)+1):
+                    p_copy=p.copy()
+                    p_copy.insert(i,n)
+                    new_perms.append(p_copy)
+            perms=new_perms
+        return perms
+    
+# TC=O(n!*n^2) (n^2 for n insert operation each of n TC)
+# SC=O(n!*n)
+
+#Solution 3
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums)==0:
+            return [[]]
+        res=[]
+        perm=self.permute(nums[1:])
+        for p in perm:
+            for i in range(len(p)+1):
+                p_copy=p.copy()
+                p_copy.insert(i,nums[0])
+                res.append(p_copy)
+        return res
+    
+# TC=O(n!*n^2) (n^2 for n insert operation each of n TC)
+# SC=O(n!*n)
+
+        
