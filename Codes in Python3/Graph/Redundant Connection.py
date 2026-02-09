746bcdc31ea665efbcac6c56e6633adc5d9370a0
abhi-sama <abhi121197@gmail.com>
2026-02-08 21:08:32 -0800

    Redundant Connection

--- Codes in Python3/Graph/Redundant Connection.py
+++ Codes in Python3/Graph/Redundant Connection.py
@@ -1,3 +1,35 @@
+#Disjoint Sets
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n=len(edges)
+        par=[i for i in range(n+1)]
+        rank=[1]*(n+1)
+
+        def find(n):
+            if n!=par[n]:
+                par[n]=find(par[n])
+            return par[n]
+
+        def union(n1,n2):
+            p1,p2=find(n1),find(n2)
+            if p1==p2:
+                return False
+            if rank[p1]>rank[p2]:
+                par[p2]=p1
+                rank[p1]+=rank[p2]
+            else:
+                par[p1]=p2
+                rank[p2]+=rank[p1]
+            return True
+
+        for n1,n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+        return []
+
+# TC=O((V+(E*alpha(V))))
+# SC=O(V)
+
 #Topological Sort (Kahn's Algorithm) Approach
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
