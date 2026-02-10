29e10ce075ecb9d51f3a3710681bd18628b936dc
abhi-sama <abhi121197@gmail.com>
2026-02-09 21:39:29 -0800

    Longest Increasing Subsequence

--- Codes in Python3/Dynamic Programming/Longest Increasing Subsequence.py
+++ Codes in Python3/Dynamic Programming/Longest Increasing Subsequence.py
@@ -1,8 +1,37 @@
+#Top Down Approach
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        t=[1]*len(nums)
-        for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[i]>nums[j]:
-                    t[i]=max(t[j]+1,t[i])
-        return max(t)
+        dp=[[-1]*(len(nums)+1) for _ in range(len(nums)+1)]
+        def f(ind,prev_ind):
+            if ind==len(nums):
+                return 0
+            if dp[ind][prev_ind]!=-1:
+                return dp[ind][prev_ind]
+            LIS= f(ind+1,prev_ind) #Dont Include Current element
+            if prev_ind==-1 or nums[ind]>nums[prev_ind]:
+                LIS=max(LIS,1+f(ind+1,ind)) #Include Current Element
+            dp[ind][prev_ind]=LIS
+            return dp[ind][prev_ind]
+        return f(0,-1)
+    
+# TC=O(n^2)
+# SC=O(n^2)
+
+#Recursive Approach
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        def f(ind,prev_ind):
+            if ind==len(nums):
+                return 0
+            
+            LIS= f(ind+1,prev_ind) #Dont Include Current element
+            if prev_ind==-1 or nums[ind]>nums[prev_ind]:
+                LIS=max(LIS,1+f(ind+1,ind)) #Include Current Element
+
+            return LIS
+
+        return f(0,-1)
+    
+# TC=O(2^(n))
+# SC=O(n)
