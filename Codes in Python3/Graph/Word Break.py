8d46112003129077c5497032acf81738cb20c731
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-02-11 17:54:17 -0800

    Word Break

--- Codes in Python3/Graph/Word Break.py
+++ Codes in Python3/Graph/Word Break.py
@@ -1,3 +1,24 @@
+#Bottom Up Solutiom
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:    
+        dp=[False] * (len(s)+1)
+        dp[len(s)]=True
+
+        for i in range(len(s)-1,-1,-1):
+            for w in wordDict:
+                size=len(w)
+                if i+size<=len(s) and s[i:i+size]==w:
+                    dp[i]=dp[i+size]
+                if dp[i]:
+                    break
+        
+        return dp[0]
+
+# TC=O(T*M*N)
+    # Let N be the length of the string and 
+    # M be the number of words in wordDict, and T max lenght of any word (as to compare)
+# SC=O(N)
+
 #Top Down Solutiom
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:    
