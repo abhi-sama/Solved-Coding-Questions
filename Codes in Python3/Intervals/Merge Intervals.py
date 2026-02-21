061eafa96128024c6d9c17176eb07918a83b2492
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-02-20 22:25:59 -0800

    Merge Intervals

---
+++ Codes in Python3/Intervals/Merge Intervals.py
@@ -0,0 +1,18 @@
+#Sorting Approach
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res=[]
+        intervals.sort()
+        res.append(intervals[0])
+        
+        for i in range(1,len(intervals)):
+            if res[-1][1]>=intervals[i][0]:
+                res[-1][1]=max(res[-1][1],intervals[i][1])
+            else:
+                res.append(intervals[i])
+        
+        return res
+    
+# TC=O(nlogn)
+# SC=O(n)
+        
