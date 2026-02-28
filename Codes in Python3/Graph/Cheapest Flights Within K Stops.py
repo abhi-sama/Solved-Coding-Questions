ad09dd203c03399c1062962e75047cd810be3bb7
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-02-27 23:38:29 -0800

    Cheapest Flights Within K Stops

--- Codes in Python3/Graph/Cheapest Flights Within K Stops.py
+++ Codes in Python3/Graph/Cheapest Flights Within K Stops.py
@@ -1,3 +1,27 @@
+#Shortest Path Faster Algorithm
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices=[float("inf")]*n
+        prices[src]=0
+        adj = [[] for _ in range(n)]
+        for u, v, cst in flights:
+            adj[u].append([v, cst])
+        
+        q=deque([(0,src,0)]) #cost,node,stops
+        while q:
+            cost,node,stops=q.popleft()
+            if stops>k:
+                continue
+            for nei,w in adj[node]:
+                if cost+w<prices[nei]:
+                    prices[nei]=cost+w
+                    q.append((cost+w,nei,stops+1))
+        
+        return -1 if prices[dst]==float('inf') else prices[dst]
+
+# TC=O(n.k)
+# SC=O(n+m)
+
 #Bellman Ford
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
