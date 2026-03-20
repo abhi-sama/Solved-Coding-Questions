fe2a70495c11876bae54c425303f95a6bccf0d8e
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-03-19 22:37:18 -0700

    Reverse Linked List

--- Codes in Python3/Linked List/Reverse Linked List.py
+++ Codes in Python3/Linked List/Reverse Linked List.py
@@ -3,13 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        curr=head
         prev=None
-        current=head
-        while current:
-            temp=current.next
-            current.next=prev
-            prev=current
-            current=temp
+        temp=None
+
+        while curr:
+            temp=curr.next
+            curr.next=prev
+            prev=curr
+            curr=temp
         return prev
+
+# TC=O(n)
+# SC=O(1)
