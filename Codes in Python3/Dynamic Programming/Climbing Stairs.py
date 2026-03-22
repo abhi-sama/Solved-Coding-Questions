0be56b45d3a7097680b79057cdf505ab2a24e599
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-03-22 09:19:27 -0700

    Climbing Stairs

---
+++ Codes in Python3/Dynamic Programming/Climbing Stairs.py
@@ -0,0 +1,56 @@
+#Using Space Optimised Approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one,two=1,1
+        for i in range(n-1):
+            temp=one
+            one=one+two
+            two=temp
+        return one
+
+# TC=O(n)
+# SC=O(n)       
+
+#Using Bottom-Up Approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        dp=[0]*(n+1)
+        dp[1],dp[2]=1,2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+
+# TC=O(n)
+# SC=O(n)       
+
+#Using Top-Down Approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache=[-1]*n
+        def dfs(i):
+            if i>=n:
+                return i==n
+            if cache[i]!=-1:
+                return cache[i]
+            cache[i]=dfs(i+1) + dfs(i+2)
+            return cache[i]
+
+        return dfs(0)
+
+# TC=O(n)
+# SC=O(n)       
+
+#Using DFS
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        def dfs(i):
+            if i>=n:
+                return i==n
+            return dfs(i+1) + dfs(i+2)
+        return dfs(0)
+
+# TC=O(2^n)
+# SC=O(n)       
