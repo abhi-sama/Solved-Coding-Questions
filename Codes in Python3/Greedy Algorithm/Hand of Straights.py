188e9f4b0321178af0b594f59c193ab91f412eb4
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-03-24 23:25:30 -0700

    Hand of Straights

---
+++ Codes in Python3/Greedy Algorithm/Hand of Straights.py
@@ -0,0 +1,44 @@
+#Using Min-Heap
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize:
+            return False
+
+        count = {}
+        for n in hand:
+            count[n] = 1 + count.get(n, 0)
+
+        minH = list(count.keys())
+        heapq.heapify(minH)
+        while minH:
+            first = minH[0]
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != minH[0]:
+                        return False
+                    heapq.heappop(minH)
+        return True       
+
+# TC=O(nlogn)
+# SC=O(n)
+
+#Sorting
+class Solution:
+    def isNStraightHand(self, hand, groupSize):
+        if len(hand) % groupSize:
+            return False
+
+        count = Counter(hand)
+        hand.sort()
+        for num in hand:
+            if count[num]:
+                for i in range(num, num + groupSize):
+                    if not count[i]:
+                        return False
+                    count[i] -= 1
+        return True
+# TC=O(nlogn)
+# SC=O(n)
