8ca3cc2bf354563e5f12608c7acdd80ba0601d70
Ethan Hunt <ethan-hunt@Ethans-MacBook-Air.local>
2026-03-29 23:39:03 -0700

    Rotate Image

---
+++ Codes in Python3/Array/Rotate Image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j],matrix[j][i]=matrix[j][i], matrix[i][j]
+
+        for i in range(len(matrix)):
+            matrix[i].reverse()
+        
+        return 
+
+# TC=O(n^2)
+# SC=O(1)
